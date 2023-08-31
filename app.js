32f10fc810608f87f3007a3db9074b177ad349c2
acidSnkj <58449502+acidSnkj@users.noreply.github.com>
2023-08-31 23:14:03 +0000

    :wrench: Routing

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 const express = require('express');
-const chalk = require('chalk');
-const debug = require('debug')('app');
-const morgan = require('morgan');
+const chalk = require('chalk'); // colors to console.log
+const debug = require('debug')('app'); // debug messages into terminal
+const morgan = require('morgan'); // HTTP request logger middleware - output the requests
+
 const path = require('path');
 const PORT = process.env.PORT;
 
 const app = express();
+const sessionsRouter = require('./src/routers/sessionsRoutes');
 
+// Middleware
 app.use(morgan('tiny'));
-app.use(express.static(path.join(__dirname, '/public/')));
+app.use(express.static(path.join(__dirname, '/public')));
 
-app.get('/', (req, res) => {});
+// Routes
+app.use('/sessions', sessionsRouter);
+
+// Set variables to our enviroment
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => {
+  res.render('index', { title: 'To my web page', data: ['a', 'b', 'c'] });
+});
 
 app.listen(3000, () => {
   debug(`server is running on port ${chalk.green(PORT)}`);
